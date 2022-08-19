36e4ddf25da523e6d77859c7d8ed34163bc7e65f
Sarcovora <evan.kuo.edu@gmail.com>
2022-08-19 18:26:34 -0500

    automated commit message

--- Arch/dotfiles/polybar/launch.sh
+++ Arch/dotfiles/polybar/launch.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
 
 # Terminate already running bar instances
-killall -q polybar
-# If all your bars have ipc enabled, you can also use
-# polybar-msg cmd quit
+# killall -q polybar
+# # If all your bars have ipc enabled, you can also use
+# # polybar-msg cmd quit
 
-# Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
+# # Launch Polybar, using default config location ~/.config/polybar/config.ini
+# polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
+
+(
+  flock 200
+
+  killall -q polybar
+
+  while pgrep -u $UID -x polybar > /dev/null; do sleep 0.5; done
+
+  outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
+  tray_output=eDP1
+
+  for m in $outputs; do
+    if [[ $m == "HDMI1" ]]; then
+        tray_output=$m
+    fi
+  done
+
+  for m in $outputs; do
+    export MONITOR=$m
+    export TRAY_POSITION=none
+    if [[ $m == $tray_output ]]; then
+      TRAY_POSITION=right
+    fi
+
+    polybar --reload mybar </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
+    disown
+  done
+) 200>/var/tmp/polybar-launch.lock
