0efdff80d4d147dadbf425fa936081263e853436
Evan Kuo <evan.kuo.edu@gmail.com>
2024-02-13 01:52:26 -0600

    added wallpaper and fonts

--- Arch/installFonts.sh
+++ Arch/installFonts.sh
@@ -7,13 +7,13 @@
 
 ## Colors ----------------------------
 Color_Off='\033[0m'
-BBlack='\033[1;30m' BRed='\033[1;31m'    BGreen='\033[1;32m' BYellow='\033[1;33m'
-BBlue='\033[1;34m'  BPurple='\033[1;35m' BCyan='\033[1;36m'  BWhite='\033[1;37m'
+BBlack='\033[1;30m' BRed='\033[1;31m' BGreen='\033[1;32m' BYellow='\033[1;33m'
+BBlue='\033[1;34m' BPurple='\033[1;35m' BCyan='\033[1;36m' BWhite='\033[1;37m'
 
 ## Directories ----------------------------
-DIR=`pwd`
-# FONT_DIR="$HOME/.local/share/fonts"
-FONT_DIR="/usr/share/fonts/sarcInstall"
+DIR=$(pwd)
+FONT_DIR="$HOME/.local/share/fonts/sarcInstall"
+# FONT_DIR="/usr/share/fonts/sarcInstall"
 
 # Install Fonts
 install_fonts() {
