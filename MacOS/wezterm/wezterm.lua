127e5fb44207f17d511323c15ebcb93ef6259e54
Evan Kuo <evan.kuo.edu@gmail.com>
2025-01-10 15:56:20 -0600

    Update wezterm.lua

--- MacOS/wezterm/wezterm.lua
+++ MacOS/wezterm/wezterm.lua
@@ -4,24 +4,58 @@ return {
 	font = wezterm.font("GeistMono Nerd Font Mono"),
 	font_size = 16.0,
 
-	color_scheme = "catppuccin-mocha",
+	max_fps = 120,
+
+	-- color_scheme = "catppuccin-mocha",
+	color_scheme = "Rosé Pine (Gogh)",
 
 	hide_tab_bar_if_only_one_tab = true,
 
 	window_padding = {
-		left = 10,
-		right = 10,
+		left = 5,
+		right = 5,
 		top = 5,
 		bottom = 5,
 	},
 
-	window_background_opacity = 0.80,
-	macos_window_background_blur = 70,
+	-- background = {
+	-- 	{
+	-- 		source = {
+	-- 			-- File = "/Users/" .. os.getenv("USER") .. "/.config/wezterm/dark-desert.jpg",
+	-- 			Color = "#1E1E2E",
+	-- 		},
+	-- 		width = "100%",
+	-- 		height = "100%",
+	-- 		opacity = 1,
+	-- 		-- hsb = {
+	-- 		-- 	hue = 1.0,
+	-- 		-- 	saturation = 1.02,
+	-- 		-- 	brightness = 0.25,
+	-- 		-- },
+	-- 		-- attachment = { Parallax = 0.3 },
+	-- 		-- width = "100%",
+	-- 		-- height = "100%",
+	-- 	},
+	-- 	{
+	-- 		source = {
+	-- 			Color = "#b541cc",
+	-- 		},
+	-- 		width = "100%",
+	-- 		height = "100%",
+	-- 		opacity = 0.2,
+	-- 	},
+	-- },
+
+	-- window_background_opacity = 0.85,
+	-- window_background_opacity = 0.80,
+	window_background_opacity = 0.95,
+	-- window_background_opacity = 1,
+	macos_window_background_blur = 40,
 
 	window_decorations = "RESIZE",
 
-	initial_rows = 30,
-	initial_cols = 100,
+	initial_rows = 40,
+	initial_cols = 130,
 
 	-- Disable ligatures
 	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
@@ -29,7 +63,7 @@ return {
 	keys = {
 		{ key = "t", mods = "CTRL", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
 		{ key = "w", mods = "CTRL", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
-		{ key = "c", mods = "CTRL", action = wezterm.action({ CopyTo = "Clipboard" }) },
-		{ key = "v", mods = "CTRL", action = wezterm.action({ PasteFrom = "Clipboard" }) },
+		-- { key = "c", mods = "CTRL", action = wezterm.action({ CopyTo = "Clipboard" }) },
+		-- { key = "v", mods = "CTRL", action = wezterm.action({ PasteFrom = "Clipboard" }) },
 	},
 }
